2a7b7ab4ef0060e605780df30822690b6ace677a
João paulo C <joaopaulo33573@gmail.com>
2022-03-17 10:37:48 -0300

    adcionar e remover produto ao carrinho

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,72 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      //Se não colocasse entre conchetes tudo que mudasse em updateCart iria mudar em cart
+      const updatedCart= [...cart];
+      //Verificando se determinado item está no array
+      const productExists= updatedCart.find(product => product.id === productId);
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount= stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if(amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return;
+      }
+
+
+      if(productExists){
+        productExists.amount = amount;
+      }else{
+      const product = await api.get(`/products/${productId}`);
+
+      const newProduct = {
+        ...product.data,
+        amount: 1
+      }
+
+      updatedCart.push(newProduct);
+    }
+    setCart(updatedCart);
+
+    localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+
+      
+
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart= [...cart];
+
+      const productRemove= updatedCart.find(product => product.id === productId);
+
+      var productOut = updatedCart.filter((product) => product.id !== productId);
+
+      setCart(productOut);
+      
+      
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(productOut));
+
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,16 +22,24 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount= {...sumAmount};
+    // Isso aqui é um objeto, não um array seria como se fosse, por exemplo: newSumAmount.5
+     newSumAmount[product.id] = product.amount;
+    return newSumAmount;
+
+    
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+       const response = await api.get('products');
+
+       const data
+     
     }
 
     loadProducts();
@@ -50,11 +58,11 @@ const Home = (): JSX.Element => {
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+        onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
+            {cartItemsAmount[product.id] || 0} 2
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
